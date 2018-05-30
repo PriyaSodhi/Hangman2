1490b9eeb79c08b15b5a69663a3faf1e8b12be9e
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-05-30 13:22:33 +1200

    Refactor in progress

--- hangman.rb
+++ hangman.rb
@@ -11,34 +11,15 @@ class Hangman
     @guesses = []
   end
 
-  def start_game
-    clue = build_clue(word, guesses)
-    game_in_progress = game_in_progress?(word, guesses, lives)
-    won = won?(word, guesses, lives)
-    lost = lost?(word, guesses, lives)
-    TurnResult.new("game_just_started", lives, guesses, clue, game_in_progress, won, lost )
-  end
-
-  def play_turn(guess)
-    TurnResult.new(
-      validate_guess(guess),
-      remaining_lives,
-      guesses,
-      build_clue(word, guesses),
-      game_in_progress?(word, guesses, remaining_lives),
-      won?(word, guesses, remaining_lives),
-      lost?(word, guesses, remaining_lives) )
-    end
-
   def validate_guess(guess)
     if !valid_guess?(guess)
       :invalid_guess
-    elsif duplicate_guess?(guess, guesses)
+    elsif duplicate_guess?(guess)
       :duplicate_guess
-    elsif guess_correct?(guess, word)
+    elsif guess_correct?(guess)
       guesses << guess
       :guess_correct
-    elsif !guess_correct?(guess, word)
+    elsif !guess_correct?(guess)
       guesses << guess
       :guess_incorrect
     end
@@ -48,7 +29,7 @@ class Hangman
     lives - (guesses - word.downcase.chars.uniq).length
   end
 
-  def guess_correct?(guess, word)
+  def guess_correct?(guess)
     word.downcase.include?(guess)
   end
 
@@ -56,34 +37,13 @@ class Hangman
     /\A[A-Za-z]\z/.match?(guess.to_s)
   end
 
-  def duplicate_guess?(guess, guesses)
+  def duplicate_guess?(guess)
     guesses.include?(guess)
   end
 
-  def game_in_progress?(word, guesses, lives)
-    if word_guessed?(word, guesses) && lives < 1
-      raise ArgumentError, "word is guessed correctly but lives are #{lives}"
-    end
-
-    !won?(word, guesses, lives) && !lost?(word, guesses, lives)
-  end
-
-  def word_guessed?(word, guesses)
-    (word.downcase.chars.uniq - guesses).empty?
-  end
-
-  def lost?(word, guesses, lives)
-    !word_guessed?(word, guesses) && lives < 1
-  end
-
-  def won?(word, guesses, lives)
-    word_guessed?(word, guesses) && lives >= 1
-  end
-
-  def build_clue(word, guesses)
+  def build_clue
     word.chars.map do |letter|
       guesses.include?(letter.downcase) ? letter : nil
     end
   end
 end
-
