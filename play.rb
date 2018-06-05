a7d3686534495fad4d95dd1b3ece2ab4ee637664
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-06-06 10:10:30 +1200

    Update play class which controls the flow of the game
    Previously play method was displaying the start of the turn before the player plays the game
    
    Now there is only one display method , display_subsequent_turn, which will display start as well as subsequent turns
    Update purpose of the class
    Rename hangman attribute to game_state
    Add start_game and play_turn method in play class
    Require turn_result and clue_builder class

--- play.rb
+++ play.rb
@@ -1,46 +1,53 @@
 require_relative 'hangman_game_state.rb'
 require_relative 'hangman_console_ui.rb'
+require_relative 'turn_result.rb'
+require_relative 'clue_builder.rb'
 require 'byebug'
 =begin
 The purpose of play class
-1. Display the initial state of the game
-2. Displays the updated state of the game after playing the turn
+1. Controls the flow of the game
 =end
 
 class Play
 
-  attr_reader :hangman, :hangman_ui
+  attr_reader :game_state, :hangman_ui
 
-  def initialize(hangman, ui)
-    @hangman = hangman
+  def initialize(game_state, ui)
+    @game_state = game_state
     @hangman_ui = ui
   end
 
   def play
-    turn_result = hangman.start_game
+    turn_result = start_game
 
     while turn_result.game_in_progress? do
-
-      hangman_ui.display_start_of_turn(turn_result.remaining_lives, turn_result.guesses, turn_result.clue)
+      hangman_ui.display_subsequent_turn(turn_result.remaining_lives, turn_result.guesses, turn_result.clue)
       guess = hangman_ui.get_guess_from_player
-      turn_result = hangman.play_turn(guess)
-
-      case turn_result.result_of_guess_state
-      when :invalid_guess
-        hangman_ui.display_invalid_guess_message(guess)
-      when :duplicate_guess
-        hangman_ui.display_duplicate_guess_message(guess)
-      when :guess_correct
-        hangman_ui.display_correct_guess_message(guess)
-      when :guess_incorrect
-        hangman_ui.display_incorrect_guess_message(guess)
-      end
+      turn_result = play_turn(guess)
+      hangman_ui.display_turn_result_message(guess, turn_result.result_of_guess_state)
     end
 
     if turn_result.won?
-      hangman_ui.display_won_message(hangman.word)
+      hangman_ui.display_won_message(game_state.word)
     elsif turn_result.lost?
       hangman_ui.display_lost_message
     end
   end
+
+  private
+  def start_game
+    clue = ClueBuilder.build_clue(game_state.word, game_state.guesses)
+    TurnResult.new("game_just_started", game_state.lives, game_state.guesses, clue, game_state.word)
+  end
+
+  def play_turn(guess)
+    guess_result = game_state.attempt_guess(guess)
+    TurnResult.new(
+      guess_result,
+      game_state.remaining_lives,
+      game_state.guesses,
+      ClueBuilder.build_clue(game_state.word, game_state.guesses),
+      game_state.word
+    )
+    end
 end
