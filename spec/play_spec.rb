7653ac4f7bede1487a7f2d8703216562b060f2c5
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-06-15 11:19:33 +1200

    Add context for incorrect, invalid and duplicate guess under #play_game
    Rename the example for context, when player guessed the correct letter
    Add the context for all the guess states

--- spec/play_spec.rb
+++ spec/play_spec.rb
@@ -47,141 +47,46 @@ describe "Play" do
         and_return(turn_result)
       end
 
-      context "and the player guessed the correct word" do
+      context "and the player guessed the correct letter" do
         let(:guess) { 's' }
 
-        it "will call display guess state and game state on hangman_ui" do
+        it "will display guess state and game state as per correct guess" do
           expect(ui).to receive(:display_guess_state).with(turn_result)
           expect(ui).to receive(:display_game_state).with(game_state)
+          play_game
+        end
+      end
+
+      context "and the player guessed the incorrect letter" do
+        let(:guess) { 'w' }
+
+        it "will display guess state and game state as per incorrect guess " do
+          expect(ui).to receive(:display_guess_state).and_return(turn_result)
+          expect(ui).to receive(:display_game_state).and_return(game_state)
+          play_game
+        end
+      end
+
+      context "and the player guessed the invalid letter" do
+        let(:guess) { '@' }
+
+        it "will display guess state and game state as per invalid guess" do
+          expect(ui).to receive(:display_guess_state).and_return(turn_result)
+          expect(ui).to receive(:display_game_state).and_return(game_state)
+          play_game
+        end
+      end
+
+      context "and the player guessed the duplicate letter" do
+        let(:guess) { 'w' }
+        let(:guesses) { ['w', 't'] }
+
+        it "will display guess state and game state as per duplicate guess" do
+          expect(ui).to receive(:display_guess_state).and_return(turn_result)
+          expect(ui).to receive(:display_game_state).and_return(game_state)
           play_game
         end
       end
     end
   end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# require_relative 'play'
-# require_relative 'hangman_console_ui'
-# require 'rspec'
-#
-# describe Play do
-#
-#   let(:ui) { instance_double(HangmanConsoleUi).as_null_object }
-#   let(:game_state) { instance_double( HangmanGameState, :word => word, :guesses => guesses, :lives => lives, :attempt_guess => attempt_guess, :remaining_lives => remaining_lives )}
-#   let(:clue_for_initial_state) { [nil] }
-#   let(:guesses) { [nil] }
-#   let(:first_turn_result) { instance_double(TurnResult, :result_of_guess_state=> result_of_initial_state, :remaining_lives => remaining_lives, :guesses=>nil, :clue => clue_for_initial_state, :game_in_progress? => game_in_progress_initial_state, :won? => false, :lost? => false )}
-#   let(:subsequent_turn_result) { instance_double(TurnResult, :result_of_guess_state=> result_of_guess_state, :remaining_lives => remaining_lives, :guesses=>guesses, :clue => clue_for_subsequent_turn, :game_in_progress? => game_in_progress_for_subsequent_turns, :won? => false, :lost? => false )}
-#
-#   subject(:hangman_game) { Play.new(game_state, ui) }
-#
-#   context "when word has one letter" do
-#     let(:word) { "a" }
-#
-#     context "and player have one life" do
-#       let(:lives) { 1 }
-#       let(:remaining_lives) { 1 }
-#
-#       context "game is started " do
-#         let(:result_of_initial_state) { "game_just_started" }
-#         let(:game_in_progress_initial_state) { false }
-#         let(:attempt_guess) { "true" }
-#
-#         before do
-#           expect(TurnResult).to receive(:new).with(result_of_initial_state, lives, guesses, clue_for_initial_state, word).and_return(first_turn_result)
-#         end
-#
-#         it "will tell the player the intiial state of the game" do
-#           hangman_game.play
-#         end
-#
-#         context "player now plays the turn" do
-#           let(:remaining_lives) { 1 }
-#           let(:game_in_progress_initial_state) { true }
-#           let(:game_in_progress_for_subsequent_turns) { false }
-#
-#           before do
-#             allow(ui).to receive(:get_guess_from_player).and_return(*guesses)
-#             expect(ui).to receive(:display_subsequent_turn).with(remaining_lives, nil , clue_for_initial_state )
-#             expect(ClueBuilder).to receive(:build_clue).with(word, guesses).and_return([nil], clue_for_subsequent_turn)
-#             expect(TurnResult).to receive(:new).with(result_of_guess_state, lives, guesses, clue_for_subsequent_turn, word).and_return(subsequent_turn_result)
-#           end
-#           context "and they guessed the correct word" do
-#             let(:result_of_guess_state) { "guess_correct" }
-#             let(:guesses) { ["a"] }
-#             let(:clue_for_subsequent_turn) { ["a"] }
-#             let(:attempt_guess) { "guess_correct" }
-#
-#             it "tells the player the result of the turn is correct" do
-#               hangman_game.play
-#            end
-#           end
-#
-#           context "and they guessed the incorrect word " do
-#             let(:result_of_guess_state) { "guess_incorrect" }
-#             let(:guesses) { ["z"] }
-#             let(:clue_for_subsequent_turn) { [nil] }
-#             let(:attempt_guess) { "guess_incorrect" }
-#
-#             it "tells the player the result of the turn is incorrect"  do
-#               hangman_game.play
-#             end
-#           end
-#
-#           context "and they guessed the invalid character" do
-#             let(:result_of_guess_state) { "invalid_guess" }
-#             let(:guesses) { ["@"] }
-#             let(:clue_for_subsequent_turn) { [nil] }
-#             let(:attempt_guess) { "invalid_guess" }
-#
-#             it "tells the player the result of the turn is invalid" do
-#               hangman_game.play
-#             end
-#           end
-#         end
-#       end
-#     end
-#   end
-# end
